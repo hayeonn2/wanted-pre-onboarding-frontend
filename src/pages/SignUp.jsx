a91cec46d32e3fa9c64bf055c74065f8f6d10f1b
hayoenn2 <dwd1205@naver.com>
2023-06-11 16:47:02 +0900

    [feat] 회원가입시 axios API 통신 연결 확인 #8

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,10 +1,24 @@
 import React, { useState } from "react";
+import axios from "axios";
+
+// const url = "https://www.pre-onboarding-selection-task.shop/";
+const url = "http://localhost:8000/";
+const api = axios.create({
+  baseURL: url,
+  Headers: {
+    "Content-Type": "application/json",
+  },
+});
 
 const SignUp = () => {
   const [validEmail, setValidEmail] = useState(false);
   const [validPassword, setValidPassword] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const isValidateEmail = (e) => {
+    setEmail(e.target.value);
+
     if (e.target.value.includes("@")) {
       setValidEmail(true);
     } else {
@@ -13,11 +27,25 @@ const SignUp = () => {
   };
 
   const isValidatePassword = (e) => {
+    setPassword(e.target.value);
+
     if (e.target.value.length >= 8) {
       setValidPassword(true);
     } else {
       setValidPassword(false);
     }
+  };
+
+  const register = async () => {
+    await api
+      .post("/auth/signup", {
+        email,
+        password,
+      })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => console.log(err));
   };
 
   return (
@@ -29,6 +57,7 @@ const SignUp = () => {
         data-testid="email-input"
         placeholder="이메일을 입력해주세요"
         onChange={isValidateEmail}
+        value={email}
       />
 
       <label htmlFor="password">비밀번호</label>
@@ -38,11 +67,14 @@ const SignUp = () => {
         data-testid="password-input"
         placeholder="비밀번호를 입력해주세요"
         onChange={isValidatePassword}
+        value={password}
       />
 
       <button
+        type="submit"
         data-testid="signup-button"
         disabled={!(validEmail && validPassword)}
+        onClick={register}
       >
         회원가입
       </button>
