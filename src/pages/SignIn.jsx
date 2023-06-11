1fcfcd2990ee7629b76aaca7ce420749ce964011
hayoenn2 <dwd1205@naver.com>
2023-06-11 16:56:52 +0900

    [feat] 로그인시 axios API 통신 연결 확인 #9

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,4 +1,52 @@
+import { useState } from "react";
+import axios from "axios";
+
+const url = "http://localhost:8000/";
+const api = axios.create({
+  baseURL: url,
+  Headers: {
+    "Content-Type": "application/json",
+  },
+});
+
 const SignIn = () => {
+  const [validEmail, setValidEmail] = useState(false);
+  const [validPassword, setValidPassword] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const isValidateEmail = (e) => {
+    setEmail(e.target.value);
+
+    if (e.target.value.includes("@")) {
+      setValidEmail(true);
+    } else {
+      setValidEmail(false);
+    }
+  };
+
+  const isValidatePassword = (e) => {
+    setPassword(e.target.value);
+
+    if (e.target.value.length >= 8) {
+      setValidPassword(true);
+    } else {
+      setValidPassword(false);
+    }
+  };
+
+  const register = async () => {
+    await api
+      .post("/auth/signin", {
+        email,
+        password,
+      })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
     <div>
       <label htmlFor="email">이메일</label>
@@ -7,6 +55,8 @@ const SignIn = () => {
         id="email"
         data-testid="email-input"
         placeholder="이메일을 입력해주세요"
+        onChange={isValidateEmail}
+        value={email}
       />
 
       <label htmlFor="password">비밀번호</label>
@@ -15,9 +65,18 @@ const SignIn = () => {
         id="password"
         data-testid="password-input"
         placeholder="비밀번호를 입력해주세요"
+        onChange={isValidatePassword}
+        value={password}
       />
 
-      <button data-testid="signin-button">로그인</button>
+      <button
+        type="submit"
+        data-testid="signin-button"
+        disabled={!(validEmail && validPassword)}
+        onClick={register}
+      >
+        로그인
+      </button>
     </div>
   );
 };
