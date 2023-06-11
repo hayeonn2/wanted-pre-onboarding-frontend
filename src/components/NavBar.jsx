135983ac1352a372d6cf8bfe7bda6be65d3d4640
hayoenn2 <dwd1205@naver.com>
2023-06-11 14:18:43 +0900

    [feat] NavBar 생성

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,19 +2,27 @@ import { Link } from "react-router-dom";
 
 export default function NavBar() {
   return (
-    <nav>
-      <ul>
+    <nav className="flex justify-center items-center">
+      <ul className="flex flex-row my-8">
         <li>
-          <Link to="/">홈</Link>
+          <Link to="/" className="ml-8 hover:text-blue-500">
+            Home
+          </Link>
         </li>
         <li>
-          <Link to="/signin">로그인</Link>
+          <Link to="/signin" className="ml-8 hover:text-blue-500">
+            LogIn
+          </Link>
         </li>
         <li>
-          <Link to="/signup">회원가입</Link>
+          <Link to="/signup" className="ml-8 hover:text-blue-500">
+            SingUp
+          </Link>
         </li>
         <li>
-          <Link to="/todo">할 일</Link>
+          <Link to="/todo" className="ml-8 hover:text-blue-500">
+            Todo
+          </Link>
         </li>
       </ul>
     </nav>
